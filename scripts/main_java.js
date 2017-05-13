c468cb2c2a6d2c23a035fea64504ee51e4b7ba1a
Tony Smith <tsmith.sounds@gmail.com>
2017-05-12 17:07:56 -0700

    Updated site, added scripts/styles

--- scripts/main_java.js
+++ scripts/main_java.js
@@ -2,43 +2,57 @@
 $(document).ready(function() {
 
 
-$("li").mouseenter(function() {
-  $(this).css("color", "#cccccc");
-  $(this).mouseleave(function() {
-    $(this).css("color", "red");
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
   });
+  
+  
+  
+// Text rotator
+  
+  
+  $('#list').textition({
+    autoplay: true,
+    interval: 3.0,
+    speed: 2.0,
+    perspective: 600
+    
+  }); 
 
   
   
-});
-
   
-  
-/*
-
-$('#test').bind('mouseenter', function() {
-    var self = $(this);
-    this.iid = setInterval(function() {
-       self.append('X');
-    }, 525);
-}).bind('mouseleave', function(){
-    this.iid && clearInterval(this.iid);
-});
-  
- 
-function run() {  
-  $(".attributes").hover(function() { 
-    var prev = $("#scroll li:first-child");
-    $.unique(prev).each(function(i) {
-      $(this).delay(i*600).slideUp(function() {
-        $(this).appendTo(this.parentNode).slideDown();
-      });
-    });
-  });
-}
-  
-window.setInterval(run,200);
-
-*/
-
 });
